ada34b64074f3d4e2a048f38a053abf821cb1104
Smaran Das <sdas1832@gmail.com>
2024-04-15 02:07:24 +0100

    atoi & myPow

--- Recursion_medium.cpp
+++ Recursion_medium.cpp
@@ -1,6 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int myAtoi(string s) {
+        long num=0;
+        int i=0;
+        bool neg=0;
+
+        while(s[i]==' ') i++;
+        
+        if(i<s.length() && s[i]=='-'){
+            neg=1;
+            i++;
+        }else if(i<s.length() && s[i]=='+'){
+            i++;
+        }
+
+
+        while(i<s.length()){
+            if(s[i]>='0' && s[i]<='9'){
+                num=(num*10)+(s[i]-'0');
+                if(neg && -num<=INT_MIN) return INT_MIN;
+                else if(!neg && num>=INT_MAX) return INT_MAX;
+            }else{
+                break;
+            }
+            i++;
+        }
+        num=(neg)?-num:num;
+        return num;
+    }
+
+
 //binary Exponentiations
 double myPow(double x, int n) {
         double ans = 1.0;
