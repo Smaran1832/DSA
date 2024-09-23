92fbbe49dc0610015457aca089e18cec7cb3435e
Smaran_1832 <sdas1832@gmail.com>
2024-09-23 16:21:51 +0100

    sliding window hard

--- sliding_window.cpp
+++ sliding_window.cpp
@@ -175,7 +175,7 @@ int maxScore(vector<int>& cardPoints, int k) {
        return subbarrayLessThanK(nums,k)-subbarrayLessThanK(nums,k-1);
     }    
 
-//longest subbarray with k unique elements;
+//longest subbarray with k unique elements; map to track k unique elements + sliding window logic
 int longestKSubstr(string s, int k) {
          unordered_map<char,int> mpp;
          int l=0,r=0,max_len=-1;
@@ -196,5 +196,96 @@ int longestKSubstr(string s, int k) {
         return max_len; 
     }
 
+
+//map to track characters of the necessary string with counter logic (only previously inserted elements may reach 0 within the map)
+string minWindow(string s, string t) {
+    unordered_map<char, int> mpp;
+
+    // Edge case where t is longer than s
+    if (t.size() > s.size()) {
+        return "";
+    }
+
+    // Frequency map for characters in t
+    for (char c : t) {
+        mpp[c]++;
+    }
+
+    int required = mpp.size(); // Number of unique characters in t that need to be present in the window
+    int l = 0, r = 0, formed = 0; // 'formed' is the number of unique characters in the current window that match the required frequency
+    int min_len = INT_MAX, min_start = 0; // Track the minimum length and start index of the window
+
+    while (r < s.size()) {
+        char c = s[r];
+        if (mpp.count(c)) {
+            mpp[c]--;
+            if (mpp[c] == 0) {
+                formed++;
+            }
+        }
+
+        // Try and contract the window till the point it ceases to be 'desirable'
+        while (l <= r && formed == required) {
+            c = s[l];
+            // Save the smallest window until now
+            if (r - l + 1 < min_len) {
+                min_len = r - l + 1;
+                min_start = l;
+            }
+
+            // The character at the position pointed by `l` is going to be removed
+            if (mpp.count(c)) {
+                if (mpp[c] == 0) {
+                    formed--;
+                }
+                mpp[c]++;
+            }
+            l++;
+        }
+
+        r++;
+    }
+
+    return min_len == INT_MAX ? "" : s.substr(min_start, min_len);
+}
+
+
+
+//minimum sliding window subsequence
+//we use 3 pointers 2 on the larger string 1 on the smaller; if the 3 pointer manages to go over the size of 
+//the second string that means we have encountered all the characters we wanted in the correct sequence. we 
+//will traverse back form the current point at string 1 to find the minimum possible string with the k third pointer
+//repeat the process untill string 1 is completely finished.
+   string minWindow(string& str1, string& str2) {
+    int i=0,j=0,k=0,res=INT_MAX;
+    int s1=str1.size(),s2=str2.size();
+    string ans="";
     
+    while(j<s1 && k<s2){
+        if(str1[j]==str2[k]){
+            k++;
+        }
+        
+        if(k==s2){
+            i=j;
+            k=s2-1;
+            while(i>=0 && k>=0){
+                if(str1[i]==str2[k]){
+                    k--;
+                }
+                i--;
+            }
+            i++;
+            if(res>j-i+1){
+                res=j-i+1;
+                ans=str1.substr(i,res);
+            }
+            k=0;
+            j=i+1;
+        }
+        
+        j++;
+    }
     
+    return ans;
+    }    
